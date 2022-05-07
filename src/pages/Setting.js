d05cfeeda1cf941c0f5f1304dd681d8cc6d0ebcb
Jwon <check4545@daum.net>
2022-05-07 21:50:12 +0900

    Implement Setting Page

---
+++ src/pages/Setting.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {ScrollView, Text} from 'react-native';
+import styled from 'styled-components';
+
+function Settings({navigation}) {
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <Text>Settings</Text>
+          <Text>Email : Example@Example.com</Text>
+          <Text>Tel : 02-***-***</Text>
+        </ScrollView>
+      </SafeAreaView>
+    )
+  }
+
+const SafeAreaView = styled.SafeAreaView`
+    flex: 1;
+`;
+
+
+export default Settings; 
