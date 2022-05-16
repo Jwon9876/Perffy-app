7ea42014664f3ee4074bfa1919cfca7c68965d8c
Jwon <check4545@daum.net>
2022-05-17 02:26:07 +0900

    Implement Profile

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,25 +1,57 @@
 import React from 'react'
-import {ScrollView, Text} from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import styled from 'styled-components';
 
-function Settings({navigation}) {
+function Setting({ navigation }) {
     return (
-      <SafeAreaView>
-        <ScrollView>
-          <Text>Settings</Text>
-          <Text>Email : Example@Example.com</Text>
-          <Text>Tel : 02-***-***</Text>
-        </ScrollView>
-      </SafeAreaView>
+        <SafeAreaView>
+            <ScrollView>
+                <ProfileView>
+                    <Profile>
+                        <ProfilePic>
+                            <ProfilePicEdit>
+
+                            </ProfilePicEdit>
+                        </ProfilePic>
+                    </Profile>
+                </ProfileView>
+            </ScrollView>
+        </SafeAreaView>
     )
-  }
+}
 
 const SafeAreaView = styled.SafeAreaView`
     flex: 1;
 `;
 
-const ProfileView = styled.TouchableOpacity`
+const ProfileView = styled.View`
+    flex: 1;
+    padding: 5px;
+`;
+
+const Profile = styled.TouchableOpacity`
+    width: 100%;
+    height: 150px;
+    border-radius: 5px;
+    border: 1px solid;
+    justify-content: center;
+`;
+
+const ProfilePic = styled.TouchableOpacity`
+    width: 120px;
+    height: 120px;
+    padding-left:10px;
+    border-radius:75px;
+    border: 1px solid;
+    margin-left: 10px;
+`;
+
+const ProfilePicEdit = styled.View`
+    background-color: red;
+    width: 50px;
+    height: 50px;
+    overflow: hidden;
 `;
 
 
-export default Settings; 
+export default Setting; 
