8f69bddd6bd342083da0433c58edd35b229002f7
Jwon <check4545@kakao.com>
2022-08-09 01:32:24 +0900

    Add Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,59 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
 import styled from 'styled-components'
+
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth'
+
 
 function Login(){
 
+
+    const googleSigninConfigure = () => {
+        GoogleSignin.configure({
+          webClientId:
+            '956427024713-p7ho1l49issvrjlvag80iubmgt8mu33v.apps.googleusercontent.com',
+        })
+      }
+
+      const onGoogleButtonPress = async () => {
+        const { idToken } = await GoogleSignin.signIn();
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+        return auth().signInWithCredential(googleCredential);
+    }
+
+      useEffect(() => {
+        googleSigninConfigure()
+      }, [])
+
+
     return(
-        <PostComponent>
-            asdasdkajsdhaksdb
-        </PostComponent>
+        <Wrapper>
+            
+            <LoginBtn
+                onPress={() => onGoogleButtonPress()}
+            >
+
+            </LoginBtn>
+
+        </Wrapper>
     )
 
 }
 
 export default Login;
 
-const PostComponent = styled.Text`
-    font-size: 12px
+const Wrapper = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
 `;
+
+const LoginBtn = styled.TouchableOpacity`
+    width: 150px;
+    height: 50px;
+    border: 1px solid;
+`   
+
+const InnerText = styled.Text`
+    font-size: 25px;
+`
