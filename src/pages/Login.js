7b0f277c50e7b55657a0b902ba2beee1fdf00fb6
Jwon <check4545@kakao.com>
2022-08-08 23:34:12 +0900

    Test

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styled from 'styled-components'
+
+function Login(){
+
+    return(
+        <PostComponent>
+            asdasdkajsdhaksdb
+        </PostComponent>
+    )
+
+}
+
+export default Login;
+
+const PostComponent = styled.Text`
+    font-size: 12px
+`;
