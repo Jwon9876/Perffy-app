d0f53a9763c2629c5251fe6f7e2920be610906b1
Jwon <check4545@kakao.com>
2022-08-11 23:53:07 +0900

    Refactoring Stack

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Login from './pages/Login'
 import OnBoardingFirstPage from './pages/OnBoarding/OnBoardingFirstPage';
 import OnBoardingSecondPage from './pages/OnBoarding/OnBoardingSecondPage';
 
-import HomeScreen from './pages/Home';
+import Home from './pages/Home';
 import Search from './pages/Search';
 import Setting from './pages/Setting'
 
@@ -26,59 +26,35 @@ import CreateReview from './pages/review/CreateReview';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-// TODO: Stack Refactoring
+const screenOptions = {
+    tabBarActiveTintColor: "#8c0485",
+    tabBarLabelStyle: {
+        fontSize: 12
+    },
+    tabBarItemStyle: {
+        height: 60
+    },
+    tabBarStyle: [
+        {
+            display: 'flex',
+            backgroundColor: "white",
+            paddingBottom: "20%"
+        },
+    ],
+}
 
-function Home() {
+
+function BottomTabBar() {
     return (
-        <Tab.Navigator initialRouteName="HomeScreen"
-            screenOptions={
-                {
-                    // "tabBarActiveTintColor": "#cb3bff",
-                    "tabBarActiveTintColor": "#8c0485",
-                    "tabBarLabelStyle": {
-                        "fontSize": 12
-                    },
-                    "tabBarItemStyle": {
-                        "height": 60
-                    },
-                    "tabBarStyle": [
-                        {
-                            "display": "flex",
-                            "backgroundColor": "white",
-                            "paddingBottom": "20%"
-                        },
-                    ],
-                }
-            }
+        <Tab.Navigator initialRouteName="Home"
+            screenOptions={screenOptions}
         >
-            <Tab.Screen name="HomeScreen" component={HomeScreen}
-                // options={{
-                //     title: '홈',
-                //     tabBarIcon: ({ color, size }) => (
-                //         // <Icon name="home" color={color} size={size} />
-                //         <Image
-                //             resizeMode = {'contain'}
-                //             source={'src/components/icons/tabBarHomeIcon.png'}
-                //         >
-
-                //         </Image>
-                //     ),
-
+            <Tab.Screen name="Home" component={Home}
                 options={{
-                    tabBarIcon: ({ focused }) => {
-                        return (
-                            <Image
-                                resizeMode={'contain'}
-                                source={
-                                    {
-                                        uri: '/Users/choejuwon/Documents/GitHub/Perffy-app/src/components/icons/tabBarHomeIcon.png'
-                                    }
-                                }
-                            //   style={styles.icon}
-                            />
-                        );
-                    },
-                    headerShown: false
+                    title: '홈',
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" color={color} size={size}/>
+                    )
                 }}
 
             />
@@ -104,36 +80,28 @@ function Home() {
     )
 }
 
+
+const OnBoardingStack = () => {
+    const Stack = createNativeStackNavigator();
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Stack.Screen name={'OnBoardingFirstPage'} component={OnBoardingFirstPage} />
+            <Stack.Screen name={'OnBoardingSecondPage'} component={OnBoardingSecondPage} />
+        </Stack.Navigator>
+    );
+};
+
 export default function App() {
     return (
+        // 하위 페이지를 여기에 계속 추가할 게 아니라, 하나의 탭바 안에 하나의 스택 단위로 묶어서 탭바안에다가 넣기
         <NavigationContainer>
-            <Stack.Navigator
-            // screenOptions={{
-            //   headerStyle: {
-            //     backgroundColor: 'black',
-            //   },
-            //   headerTintColor: '#fff',
-            //   headerTitleStyle: {
-            //     fontWeight: 'bold',
-            //   },
-            // }}
-            >
+            <Stack.Navigator>
                 {/* <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} /> */}
-
-
-
-                {/* <Stack.Screen name="OnBoardingFirstPage" component={OnBoardingFirstPage} options={{ headerShown: false }} /> */}
-                {/* <Stack.Screen name="OnBoardingSecondPage" component={OnBoardingSecondPage} options={{ headerShown: false }} /> */}
-
-
-
-
-                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-                <Stack.Screen name="Search" component={Search} options={{ headerShown: false }} />
-                <Stack.Screen name="Setting" component={Setting} />
-                <Stack.Screen name="Review" component={Review} options={{ title: "리뷰", headerShown: true }} />
-                <Stack.Screen name="ReviewDetail" component={ReviewDetail} options={{ title: "리뷰", headerShown: true }} />
-                <Stack.Screen name="CreateReview" component={CreateReview} options={{ title: "리뷰 작성", headerShown: false, }} />
+                <Stack.Screen name="OnBoarding" component={OnBoardingStack} options = {{headerShown: false}}/>
+                <Stack.Screen name="BottomTabBar" component={BottomTabBar} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
