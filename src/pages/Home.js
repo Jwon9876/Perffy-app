c1c8b7f481a1790e1326ed04555ceb3604214bfc
Jwon <check4545@kakao.com>
2022-08-12 00:40:24 +0900

    Add navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,30 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Image } from 'react-native';
 
 import styled from "styled-components";
 
 
+function Home({navigation}){
 
-function Home(){
-
-    const androidStyle = {
-        paddingBottom: 500
-    }
-
-    const iosStyle = {
-
-    }
+    // navigation.setOptions({ tabBarVisible: true })
 
     return(
         <SafeAreaView>
-
+            <ScrollView>
+                <SearchBarView>
+                    <SearchBar
+                        onPress = {() => navigation.navigate('SeachPage') }
+                    >
+                        <Image
+                            style = {{marginRight: 5}}
+                            source={require('../components/icons/searchIcon.png')}
+                        />         
+                        <SearchBarInnerText>
+                            향수 이름, 브랜드, 향기 등으로 찾기
+                        </SearchBarInnerText>
+                    </SearchBar>
+                </SearchBarView>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -25,7 +32,35 @@ function Home(){
 
 const SafeAreaView = styled.SafeAreaView`
     flex: 1;
+    background-color: #FFFFFF;
 `;
+
+const ScrollView = styled.ScrollView`
+    flex: 1;
+`;
+
+const SearchBarView = styled.TouchableOpacity`
+    justify-content: center;
+    align-items: center;
+    padding-top: 15px;
+`;
+
+const SearchBar = styled.TouchableOpacity`
+    width: 90%;
+    height: 42px;
+    /* border: 1px solid; */
+    /* justify-content: center; */
+    align-items: center;
+    padding-left: 10px;
+    background-color: #F8F8F8;
+    flex-direction: row;
+`;
+
+const SearchBarInnerText = styled.Text`
+    font-size: 14px;
+    font-weight: 500;
+`;
+
 
 
 
