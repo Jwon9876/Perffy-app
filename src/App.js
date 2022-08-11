613c26e5ce5d74b41dbe167e8203f2a2359b7917
Jwon <check4545@kakao.com>
2022-08-12 00:39:57 +0900

    Refactoring

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ import OnBoardingFirstPage from './pages/OnBoarding/OnBoardingFirstPage';
 import OnBoardingSecondPage from './pages/OnBoarding/OnBoardingSecondPage';
 
 import Home from './pages/Home';
+import SeachPage from './pages/SeachPage';
+
+
 import Search from './pages/Search';
 import Setting from './pages/Setting'
 
@@ -49,14 +52,14 @@ function BottomTabBar() {
         <Tab.Navigator initialRouteName="Home"
             screenOptions={screenOptions}
         >
-            <Tab.Screen name="Home" component={Home}
+            <Tab.Screen name="HomeStack" component={HomeStack}
                 options={{
                     title: '홈',
                     tabBarIcon: ({ color, size }) => (
                         <Icon name="home" color={color} size={size}/>
-                    )
+                    ),
+                    headerShown: false
                 }}
-
             />
             <Tab.Screen name="검색" component={Search}
                 options={{
@@ -94,13 +97,27 @@ const OnBoardingStack = () => {
     );
 };
 
+const HomeStack = ({navigation, route}) => {
+    const Stack = createNativeStackNavigator();
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Stack.Screen name={'Home'} component={Home} />
+            <Stack.Screen name={'SeachPage'} component={SeachPage}/>
+        </Stack.Navigator>
+    );
+};
+
+
 export default function App() {
     return (
         // 하위 페이지를 여기에 계속 추가할 게 아니라, 하나의 탭바 안에 하나의 스택 단위로 묶어서 탭바안에다가 넣기
         <NavigationContainer>
             <Stack.Navigator>
                 {/* <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} /> */}
-                <Stack.Screen name="OnBoarding" component={OnBoardingStack} options = {{headerShown: false}}/>
+                {/* <Stack.Screen name="OnBoarding" component={OnBoardingStack} options = {{headerShown: false}}/> */}
                 <Stack.Screen name="BottomTabBar" component={BottomTabBar} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
