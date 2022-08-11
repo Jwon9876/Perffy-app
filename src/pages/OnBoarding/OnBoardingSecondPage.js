bc7e53a7bcaa6254824ceb617ae77691dd6789cd
Jwon <check4545@kakao.com>
2022-08-11 22:45:19 +0900

    Implement OnBoardingSecondPage, validation Check

--- src/pages/OnBoarding/OnBoardingSecondPage.js
+++ src/pages/OnBoarding/OnBoardingSecondPage.js
@@ -1,8 +1,59 @@
-import React from "react";
+import React, {useState} from "react";
+import { Alert } from "react-native";
 
 import styled from "styled-components";
 
 function OnBoardingSecondPage({navigation}){
+
+    const perfumeTag = ["우디", "시트러스", "머스크", "레더", "플로럴", "그린", "아쿠아", "오리엔탈"];
+    const [selectedTagList, setSelectedTagList] = useState([]);
+
+    const notPressdTagStyle = {
+        fontSize:12, 
+        backgroundColor: "#EFEFEF",
+        marginTop: 12,
+        marginLeft:5,
+        padding: 3
+    }
+
+    const notPressdTextStyle = {
+        marginLeft: 0,
+        fontSize: 13,
+        color: "#666666"
+    }
+
+    const pressdTagStyle = {
+        fontSize:12, 
+        backgroundColor: "#5ABACA",
+        marginTop: 12,
+        marginLeft:5,
+        padding: 3
+    }
+
+    const pressdTextStyle = {
+        marginLeft: 0,
+        fontSize: 13,
+        color: "#FFFFFF"
+    }
+
+    function insertTag(tag) {
+        if (selectedTagList.some((v) => v == tag)) {
+            setSelectedTagList(selectedTagList.filter(v => v !== tag));
+            return
+        }
+        setSelectedTagList([...selectedTagList, tag])
+    }
+
+    function validationCheck(){
+        if(3 > selectedTagList.length){
+            Alert.alert("태그를 3개 이상 선택해주세요");
+        } else{
+            console.log(selectedTagList)
+            navigation.navigate('Home')
+        }
+    }
+
+    
 
     return(
         <SafeAreaView>
@@ -12,10 +63,60 @@ function OnBoardingSecondPage({navigation}){
                 <PhasePoint />
             </PhaseView>
 
+            <TitleText>
+                관심 향수를 선택해주세요
+            </TitleText>
+
+            <DescriptionnText
+                style = {{fontSize:12, marginTop: 30}}
+            >
+                * 3가지 이상 선택해주세요
+            </DescriptionnText>
+            <PerfumeTagView>
+
+                {
+                    perfumeTag.map((v, i) => 
+                        <PerfumeTag key={v}
+                            // style = {{marginTop: 12, marginLeft:5, padding: 3}}
+                            style = {                         
+                                (
+                                    selectedTagList.includes(v)
+                                ) ? (
+                                    pressdTagStyle
+                                ) : (
+                                    notPressdTagStyle
+                                )
+                            }
+                            onPress = {() => insertTag(v)}
+                        >
+                            <DescriptionnText
+                                style = {
+                                    (
+                                        selectedTagList.includes(v)
+                                    ) ? (
+                                        pressdTextStyle
+                                    ) : (
+                                        notPressdTextStyle
+                                    )
+                                }
+
+                                
+                            >
+                                {v}
+                            </DescriptionnText>
+                        </PerfumeTag>
+                    )
+
+                    
+                }
+
+            </PerfumeTagView>
+
+
             <FooterView
             >
                 <PreviousBtn
-                    style = {{marginBottom: 3}}
+                    style = {{marginBottom: 5}}
                     onPress={() => navigation.goBack()}
                 >
                     <PreviousBtnText>
@@ -24,7 +125,7 @@ function OnBoardingSecondPage({navigation}){
                 </PreviousBtn>
 
                 <NextBtn
-                    onPress={() => console.log("TODO")}
+                    onPress={() => validationCheck()}
                 >
                     <NextBtnText>
                         다음 단계
@@ -37,6 +138,20 @@ function OnBoardingSecondPage({navigation}){
 
 const SafeAreaView = styled.SafeAreaView`
     flex: 1;
+`;
+
+const TitleText = styled.Text`
+    font-size: 20px;
+    font-weight: bold;
+    margin-left: 16px;
+`;
+
+const DescriptionnText = styled.Text`
+    font-size: 10px;
+    /* font-weight: 500; */
+    margin-left: 16px;
+    margin-top: 1px;
+    color: #9E9E9E;
 `;
 
 const PhaseView = styled.View`
@@ -102,6 +217,27 @@ const PreviousBtnText = styled.Text`
     font-size: 14px;
     font-weight: 500;
     color: #FFFFFF;
+`;
+
+
+const PerfumeTagView = styled.View`
+    flex-wrap: wrap;
+    flex-direction: row;
+    padding: 10px 10px;
+    justify-content: flex-start;
+`;
+
+const PerfumeTag = styled.TouchableOpacity`
+    /* border: 1px solid; */
+    border-radius:7px;
+    padding: 2px;
+    margin: 2px;
+    align-items: center;
+    justify-content: center;
+    inline-size: block;
+    min-width: 50px;
+    width: auto;
+    height: 30px;
 `;
 
 export default OnBoardingSecondPage;
