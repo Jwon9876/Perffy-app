38ea709ce35591eb45106e9305408dbd96d001bd
Jwon <check4545@kakao.com>
2022-09-09 18:22:45 +0900

    Add SearchPage UI

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -10,6 +10,10 @@ import BackArrowIcon from '../components/icons/BackArrowIcon.svg'
 
 
 function SearchPage({ navigation }) {
+
+    const tempRecentKeywordData = ["조말론", "딥디크", "바이레도"];
+    const tempRecommnadationTagData = ["#비 오는 날", "#여름", "#겨울", "#데이트"];
+
     return (
 
         <SafeAreaView>
@@ -36,10 +40,48 @@ function SearchPage({ navigation }) {
                     </SearchBarInnerText>
                 </SearchBar>
             </SearchBarView>
+
             <ScrollView>
-                <Text>
-                    asdaasdasdasd
-                </Text>
+                <RecentSearchKeywordView>
+
+                    <RecentSearchKeywordViewTitle>
+                        최근 검색어
+                    </RecentSearchKeywordViewTitle>
+                    {
+                        tempRecentKeywordData.map((v, i) => (
+                            <RecentSearchKeywordBtn
+                                key={i}
+                            >
+                                <RecentSearchKeyword>
+                                    {v}
+                                </RecentSearchKeyword>
+
+                                <RecentSearchKeyword>
+                                    X
+                                </RecentSearchKeyword>
+                            </RecentSearchKeywordBtn>
+                        ))
+                    }
+                </RecentSearchKeywordView>
+
+                <RecommendationTagView>
+                    <RecommendationTagViewTitle>
+                        추천 태그
+                    </RecommendationTagViewTitle>
+                    <RecommendationEnumView>
+                        {
+                            tempRecommnadationTagData.map((v, i) => (
+                                <RecommendationTagBtn
+                                    key={i}
+                                >
+                                    <RecommendationTag>
+                                        {v}
+                                    </RecommendationTag>
+                                </RecommendationTagBtn>
+                            ))
+                        }
+                    </RecommendationEnumView>
+                </RecommendationTagView>
             </ScrollView>
         </SafeAreaView>
     )
@@ -80,8 +122,69 @@ const SearchBarInnerText = styled.Text`
 `;
 
 const BackArrowBtn = styled.TouchableOpacity`
-
 `;
+
+const RecentSearchKeywordView = styled.View`
+`;
+
+const RecentSearchKeywordViewTitle = styled.Text`
+    font-size: 18px;
+    font-weight: 500;
+    margin-top: 30px;
+    margin-left: 22px;
+`;
+
+const RecentSearchKeyword = styled.Text`
+    font-size: 16px;
+    color: #666666;
+`;
+
+// TODO Button -> Btn
+const RecentSearchKeywordBtn = styled.TouchableOpacity`
+    margin-top: 15px;
+    margin-left: 22px;
+    margin-right: 22px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const RecommendationTagView = styled.View`
+`;
+
+const RecommendationEnumView = styled.View`
+    padding-left: 22px;
+    padding-top: 15px;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+const RecommendationTagViewTitle = styled.Text`
+    font-size: 18px;
+    font-weight: 500;
+    margin-top: 30px;
+    margin-left: 22px;
+`;
+
+const RecommendationTag = styled.Text`
+    font-size: 14px;
+    font-weight: 500;
+    color: #FFA1B2;
+    margin: 0 8px 0 8px;
+`;
+
+const RecommendationTagBtn = styled.TouchableOpacity`
+    min-width: 50px;
+    height: 32px;
+    border: 1px solid;
+    border-color: #FFA1B2;
+    border-radius: 16px;
+    margin-right: 7px;
+    align-items: center;
+    justify-content: center;
+    
+`;
+
 
 
 export default SearchPage; 
